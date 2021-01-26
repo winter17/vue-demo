a00102239deb6383093ccdcd91d36a8d0d0fd889
liusq <shqi_liu@outlook.com>
2021-01-26 18:52:20 +0800

    only for test in my first time to learn vue.

---
+++ src/components/views/First.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="first-app">
+  {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'First',
+  data () {
+    return {
+      msg: 'Welcome to FirstApp9911'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
